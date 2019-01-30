efcc7c0848f08d401e3e1f78d3a97a3c62ae0ccc
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-01-30 16:20:50 +0000

    increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import re
 
 setup(
 	name='Python video annotator - module - tracking',
-	version=0.0,
+	version=0.1,
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
