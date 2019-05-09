5e3e2478764a085477adae09ccab1db93d142ade
Manuel Manso <manel_manso@hotmail.com>
2019-05-09 14:15:56 +0100

    Add README to pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@
 from setuptools import setup, find_packages
 import re
 
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
 setup(
 	name='Python video annotator - module - tracking',
-	version="0.3",
+	version="0.4",
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
 	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-tracking',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
 	packages=find_packages(),	
 )
