3b6f2e9b8468c9d7367c84065868ada6da23dbdb
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-07-12 14:04:04 +0100

    Update versioning system

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ import re
 with open('README.md', 'r') as fd:
     long_description = fd.read()
 
+import os, re;
+with open(os.path.join(os.path.dirname(__file__), 'pythonvideoannotator_module_tracking','__init__.py')) as fd:
+	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+
 setup(
 	name='Python video annotator - module - tracking',
-	version="0.5",
+	version=version,
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
